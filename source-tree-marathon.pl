7fe1f7b5a5cd1686b369f2e68b89fc5d27e9bf17
bennie@cpan.org <bennie@cpan.org>
2014-04-01 11:14:47 -0700

    Moving status output to STDERR so you can direct STDOUT to save the report.

--- source-tree-marathon.pl
+++ source-tree-marathon.pl
@@ -24,7 +24,7 @@ if ( $ARGV[0] and -d $ARGV[0] ) {
 }
 
 unless ( $base_dir ) {
-  print STDERR "Usage: ./source-tree-marathon.pl /source/directory/to/crawl\n";
+  print STDERR "Usage: ./source-tree-marathon.pl /source/directory/to/crawl > report.txt\n";
   exit 1;
 }
 
@@ -39,9 +39,9 @@ my $skip_dirs_regex = qr/^(\.git|tpc|debian|linux-kernel)/;
 my %data;
 
 if ( -f $dbfile ) {
-  print "Reusing file: $dbfile\n";
+  print STDERR "Reusing file: $dbfile\n";
 } else {
-  print "Creating file: $dbfile\n";
+  print STDERR "Creating file: $dbfile\n";
 }
 
 my $ref = tie %data, 'DB_File', $dbfile;
@@ -53,7 +53,7 @@ my $akm = new Acme::KeyboardMarathon;
 # Remove file stats for missing files
 for my $file ( keys %data ) {
   next if -f $file;
-  print "DEL: $file\n";
+  print STDERR "DEL: $file\n";
   delete $data{$file}
 }
 
@@ -67,11 +67,11 @@ my $cache = 0;
 
 find( \&check_stats, $base_dir );
 
-print "ADD: $add\nCACHE: $cache\nSKIP: $skip\n";
+print STDERR "ADD: $add\nCACHE: $cache\nSKIP: $skip\n";
 
 
 if ( $add ) {
-  print "\nSyncing...\n";
+  print STDERR "\nSyncing...\n";
   $ref->sync();
 }
 
@@ -115,42 +115,40 @@ sub check_stats {
   if ( -d $_ ) {
     my $localdir = $File::Find::name;
     $localdir = $1 if $localdir =~ /^$base_dir\/(.+)$/;
-    print "DIR: $localdir\n";
+    print STDERR "DIR: $localdir\n";
     return;
   }
-  $skip++ and print "SKIP: $_ (regex)\n" and return if $_ =~ /$skip_file_extension_regex/i;
+  $skip++ and print STDERR "SKIP: $_ (regex)\n" and return if $_ =~ /$skip_file_extension_regex/i;
 
   my $localdir = $File::Find::dir;
   $localdir = $1 if $localdir =~ /^$base_dir\/(.+)$/;
-  $skip++ and print "SKIP: $localdir (directory)\n" and return if $localdir =~ /$skip_dirs_regex/;
+  $skip++ and print STDERR "SKIP: $localdir (directory)\n" and return if $localdir =~ /$skip_dirs_regex/;
 
-  $skip++ and print "SKIP: $_ (binary)\n" and return if -B $File::Find::name;
-  $skip++ and print "SKIP: $_ (zero size)\n" and return if -z $File::Find::name;
+  $skip++ and print STDERR "SKIP: $_ (binary)\n" and return if -B $File::Find::name;
+  $skip++ and print STDERR "SKIP: $_ (zero size)\n" and return if -z $File::Find::name;
 
   my @stat = stat($File::Find::name);
   my $mtime = $stat[9];
   my $size  = $stat[7];
 
   if ( defined $data{$File::Find::name} and $data{$File::Find::name} =~ /^$mtime\:$size\:/ ) {
-    $cache++ and print "CACHE: $_\n";
+    $cache++ and print STDERR "CACHE: $_\n";
     return;
   }
 
-  $add++ and print "ADD: $_ ";
+  $add++ and print STDERR "ADD: $_ ";
 
   my $text = read_file($File::Find::name);
   my $dist = $akm->distance($text);
 
   $data{$File::Find::name} = "$mtime:$size:$dist";
 
-  print "(".display($dist).")\n";
+  print STDERR "(".display($dist).")\n";
 
   unless ( $add % 100 ) {
-    print "syncing...\n";
+    print STDERR "syncing...\n";
     $ref->sync();
   }
-  
-  #exit if $add > 25;
 }
 
 sub display {
